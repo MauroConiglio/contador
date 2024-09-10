8a9fc26c2353beaa14033a9d1523159b0d786050
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-09-10 12:51:30 -0300

    agregue bootstrap en mi codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
  
 
   return (
-   <h1>hola mundo</h1>
+    <main className='container my-4'>
+      <h1 className='text-center'>Primer proyecto React</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ratione vitae, fugiat harum accusamus quaerat ipsum deleniti labore adipisci repellat. Incidunt quo quae nobis. Commodi esse sint quo, nulla, debitis a quaerat dignissimos eum illo, nobis aliquam voluptates! Atque dicta quisquam quasi rerum labore tenetur, corrupti deserunt architecto non eligendi!</p>
+    </main>
+   
   )
 }
 
