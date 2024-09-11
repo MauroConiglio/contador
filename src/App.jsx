710734eaa18a5fcd2efbf1379fc292e92350a9fc
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-09-11 11:56:04 -0300

    agregamos texto mediante funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Informacion from './components/Informacion.jsx'
 function App() {
  
 
   return (
     <main className='container my-4'>
       <h1 className='text-center'>Primer proyecto React</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ratione vitae, fugiat harum accusamus quaerat ipsum deleniti labore adipisci repellat. Incidunt quo quae nobis. Commodi esse sint quo, nulla, debitis a quaerat dignissimos eum illo, nobis aliquam voluptates! Atque dicta quisquam quasi rerum labore tenetur, corrupti deserunt architecto non eligendi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ratione
+         vitae, fugiat harum accusamus quaerat ipsum deleniti labore adipisci 
+         repellat. Incidunt quo quae nobis. Commodi esse sint quo, nulla,
+          debitis a quaerat dignissimos eum illo, nobis aliquam voluptates! 
+          Atque dicta quisquam quasi rerum labore tenetur, corrupti deserunt
+           architecto non eligendi!</p>
+          <Informacion></Informacion>
     </main>
    
   )
---
+++ src/components/Informacion.jsx
@@ -0,0 +1,17 @@
+const Informacion = () => {
+// aqui agrego la mayor parte de logica
+
+    return (
+        //algo de logica
+        <section>
+            {/*aqui agrego comentario del maquetado*/}
+            <h2>Introduccion a React</h2>
+            <p> lskdgfmsld;knasdlknadsflk;asnd
+                faskjdfnaskdfnadakjsdfnadskjfbalsdnfhsdkfjgndskfjgnsdfkgsdn
+                fkgjdsfngkjlsdfgnlskdfjlksfmgsdfk;lgnsdkfjsdnflkdfsng
+            </p>
+        </section>
+    );
+};
+
+export default Informacion;
