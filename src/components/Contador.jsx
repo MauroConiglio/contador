607d3eac56aff5e76965e0ad64c6d873d3841250
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-09-13 19:56:32 -0300

    agregue boton de sumar y restar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 const Contador = () => {
+
+//let numero = 1;
+
+//crear un estado
+const [numero, setNumero] = useState(1)
+
+const sumar = ()=> {
+    setNumero(numero + 1)
+    //console.log(numero)
+}
+const restar =()=>{
+    setNumero(numero - 1)
+
+}
     return (
         <section className="text-center my-5">
             <h2>Contador</h2>
-            <h4>10</h4>
-            <button className="btn btn-primary me-2">+1</button>
-            <button className="btn btn-warning">-1</button>
+            <h4>{numero}</h4>
+            <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
+            <button className="btn btn-warning" onClick={restar}>-1</button>
         </section>
     );
 };
