0982bd8e3ba85d19e144f55b76ebac2c1ebfb59c
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-09-13 20:06:48 -0300

    edite el boton restar de una manera mas corta

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -10,16 +10,12 @@ const sumar = ()=> {
     setNumero(numero + 1)
     //console.log(numero)
 }
-const restar =()=>{
-    setNumero(numero - 1)
-
-}
     return (
         <section className="text-center my-5">
             <h2>Contador</h2>
             <h4>{numero}</h4>
             <button className="btn btn-primary me-2" onClick={sumar}>+1</button>
-            <button className="btn btn-warning" onClick={restar}>-1</button>
+            <button className="btn btn-warning" onClick={()=>setNumero(numero - 1)}>-1</button>
         </section>
     );
 };
